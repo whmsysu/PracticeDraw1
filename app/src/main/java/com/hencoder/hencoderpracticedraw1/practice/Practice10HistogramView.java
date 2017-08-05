5f4bb59ad127842542d2534258f66ae4e01be775
Haomin Wu <wuhaominsysu@gmail.com>
2017-08-05 05:20:56 -0400

    draw Histogram

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,11 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice10HistogramView extends View {
+    private Paint paint = new Paint();
 
     public Practice10HistogramView(Context context) {
         super(context);
@@ -26,5 +29,31 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        drawCoordinate(canvas);
+        drawHistogram(canvas);
+    }
+
+    // 画坐标系
+    private void drawCoordinate(Canvas canvas) {
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(2);
+        float[] pts = {200, 50, 200, 600, 200, 600, 1200, 600};
+        canvas.drawLines(pts, paint);
+    }
+
+    private void drawHistogram(Canvas canvas) {
+        float interval = 40;// 每个直方图的间隔
+        float width = 100;// 直方图的宽
+        float[] nums = {0,20,20,200,400,500,200};
+        String[] names = {"Froyo","GB","ICS","JB", "KitKat", "L", "M"};
+        paint.setColor(Color.GREEN);
+        float cordinateX = 200 + interval;
+        for (int i=0;i<nums.length;i++){
+            canvas.drawRect(cordinateX, 600 - nums[i], cordinateX + width , 600, paint);
+            paint.setTextSize(25);
+            canvas.drawText(names[i], cordinateX+30, 600 + 40, paint);
+            cordinateX+=width+interval;
+        }
+
     }
 }
